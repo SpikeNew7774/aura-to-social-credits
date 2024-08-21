efb20a70d9a66e1a58ca3fe204e2cfff46ae7968
SpikeNew7774 <newadm159@gmail.com>
2024-08-21 18:25:03 +0200

    First Publish

---
+++ src/script.js
@@ -0,0 +1,19 @@
+function convertToSocialCredits() {
+    const aura = parseFloat(document.getElementById("auraInput").value);
+    if (!isNaN(aura)) {
+        const socialCredits = (aura / 100) * 15;
+        document.getElementById("socialCreditsInput").value = socialCredits.toFixed(2);
+    } else {
+        document.getElementById("socialCreditsInput").value = "";
+    }
+}
+
+function convertToAura() {
+    const socialCredits = parseFloat(document.getElementById("socialCreditsInput").value);
+    if (!isNaN(socialCredits)) {
+        const aura = (socialCredits / 15) * 100;
+        document.getElementById("auraInput").value = aura.toFixed(2);
+    } else {
+        document.getElementById("auraInput").value = "";
+    }
+}
